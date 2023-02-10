a61df84c5aef464291f7fbbbddd3a9b45c2ecc42
bcomeau7 <baileycomeau@gmail.com>
2023-02-10 09:38:38 -0500

    added tileset for doors

---
+++ maps/Celiana-doors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Celiana-doors" tilewidth="32" tileheight="32" tilecount="72" columns="9">
+ <image source="../../space-station-office/src/assets/Celiana-doors.png" width="288" height="256"/>
+</tileset>
